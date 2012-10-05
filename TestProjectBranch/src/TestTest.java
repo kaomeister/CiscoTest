31871174096446e2232b80c75954a2ee35811dc8
Jason Kao <kaomeister@gmail.com>
2012-10-05 13:46:25 -0400

    Add print new line

--- TestProjectBranch/src/TestTest.java
+++ TestProjectBranch/src/TestTest.java
@@ -6,5 +6,10 @@ public class TestTest {
 	public void printmessage()
 	{
 		System.out.println("Just checking if this works");
+		//Comment edit
+		
+		
+		
+		System.out.println("Another line to print");
 	}
 }
